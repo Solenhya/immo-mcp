e37342a6c6aab67a26ad0bfa40f434ca0ca165b5
Fabien Herry <fabien.h@my-serious-game.com>
2026-04-07 16:21:17 +0200

    fix: :bug: fix base message/dict
    Utilisation de base message pour stocker les messages plutot qu'un mix avec dict

--- langchain_manager.py
+++ langchain_manager.py
@@ -1,7 +1,7 @@
 import asyncio
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_mistralai import ChatMistralAI
-from langchain_core.messages import AIMessage, ToolMessage
+from langchain_core.messages import AIMessage, ToolMessage , HumanMessage, SystemMessage
 from langchain_core.tools import BaseTool
 from langgraph.graph import StateGraph, MessagesState, END
 from dotenv import load_dotenv
@@ -106,7 +106,7 @@ async def main():
     langchain_manager = await LangchainManager.create(model_name="mistral-large-latest")
     prompt = f"""Tu es un assistant d'estimation de prix """
     initial_messages = [
-        {"role": "system", "content": prompt}
+        SystemMessage(prompt)
     ]
     running = True
     while running:
@@ -115,7 +115,7 @@ async def main():
             running = False
             continue
 
-        initial_messages.append({"role": "user", "content": user_input})
+        initial_messages.append(HumanMessage(user_input))
         response = await langchain_manager.run(initial_messages)
         initial_messages = list(response)
 
